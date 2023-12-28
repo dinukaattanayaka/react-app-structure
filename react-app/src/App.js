39900572baf672258e0c3b0686f1358573d33263
dinuka <dinukaattanayaka222@gmail.com>
2023-12-28 17:31:59 +0530

    before children

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,36 +1,56 @@
 import './App.css';
+
+const User1 ={
+    avatar:"https://4.bp.blogspot.com/-Uo8xiNd56fU/ThKcz-IWW2I/AAAAAAAAMus/K2utCrUZIaA/s1600/Sanath-Jayasuriya-raises.jpg",
+    name: "Sanath Jayasuriya",
+    country :"Sri Lanka",
+    runs :"13,430 runs"
+}
+const User2 ={
+    avatar:"https://i2-prod.mirror.co.uk/incoming/article834359.ece/ALTERNATES/s1200c/brian-lara.jpg",
+    name: "Brian Lara",
+    country :"West Indies",
+    runs :"10,405 runs"
+}
+const User3 ={
+    avatar:"https://i1.wp.com/im.indiatimes.in/content/2020/Jan/1_5e33f98fc7cd6.jpg?strip=all",
+    name: "Sachin Tendulkar",
+    country :"India",
+    runs :"18,426 runs"
+}
+
 function UserManager(){
   return(
       <div className='wrapper'>
-        <User avatar= "https://4.bp.blogspot.com/-Uo8xiNd56fU/ThKcz-IWW2I/AAAAAAAAMus/K2utCrUZIaA/s1600/Sanath-Jayasuriya
-      -raises.jpg" name="Sanath Jayasuriya" country="Sri Lanka" runs="1200 runs"/>
-        <User avatar= "https://i2-prod.mirror.co.uk/incoming/article834359.ece/ALTERNATES/s1200c/brian-lara.jpg"
-            name="Brian Lara" country="West Indies" runs="14000 runs"/>
-        <User/>
+        <User avatar={User1.avatar} name={User1.name} country={User1.country} runs={User1.runs}/>
+        <User avatar={User2.avatar} name={User2.name} country={User2.country} runs={User2.runs}/>
+        <User avatar={User3.avatar} name={User3.name} country={User3.country} runs={User3.runs}/>
       </div>
   );
 }
 const User =(props)=>{
+    const {avatar, name, country, runs}=props;
   return(
       <div className='user-outer'>
-          <Avatar avatar = {props.avatar}/>
-          <UserData name={props.name} country={props.country} runs={props.runs}/>
+          <Avatar avatar = {avatar}/>
+          <UserData name={name} country={country} runs={runs}/>
       </div>
   );
 }
-const Avatar =(props)=>{
+
+const Avatar =({avatar})=>{
   return(
       <div className='avatar-outer'>
-      <img src= {props.avatar}/>
+      <img src= {avatar} alt=" from google"/>
      </div>
   );
 }
-const UserData =(props)=>{
+const UserData =({name, country, runs})=>{
     return(
         <div className='user-data'>
-            <UserName name ={props.name}/>
-            <Country country ={props.country}/>
-            <Runs runs = {props.runs}/>
+            <UserName name ={name}/>
+            <Country country ={country}/>
+            <Runs runs = {runs}/>
         </div>
 
     );
@@ -43,15 +63,15 @@ const UserName =(props)=>{
       <h1 style = {inlineStyle}>{props.name}</h1>
   );
 }
-const Country =(props)=>{
+const Country =({country})=>{
     return(
-        <h2 style = {{fontStyle:'italic'}}>{props.country}</h2>
+        <h2 style = {{fontStyle:'italic'}}>{country}</h2>
     );
 }
 //const runs='12000 runs'
-const Runs =(props)=>{
+const Runs =({runs})=>{
     return(
-        <h3 style = {{fontStyle:'italic'}}>{props.runs}</h3>
+        <h3 style = {{fontStyle:'italic'}}>{runs}</h3>
     );
 }
 export default UserManager;
